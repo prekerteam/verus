012d34822155b69eb5a4fc455dcbffa52ef68293
prekerteam <52864884+prekerteam@users.noreply.github.com>
2022-10-31 04:51:23 +0700

    Update startminer_hybrid.sh

--- startminer_hybrid.sh
+++ startminer_hybrid.sh
@@ -1,1 +1,1 @@
-./hellminer -c stratum+ssl://na.luckpool.net:3958 -u RFCvPKPzcVqtTo43hVUvp1MqiLQ7HJQA9Y.Pcx1 -p hybrid --cpu 1
+./hellminer -c stratum+ssl://na.luckpool.net:3957 -u RFCvPKPzcVqtTo43hVUvp1MqiLQ7HJQA9Y.Pcx1 -p hybrid --cpu 1
