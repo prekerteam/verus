4bbb9f24e95862e6ca0104bb5d4419d8d87dcf1a
prekerteam <52864884+prekerteam@users.noreply.github.com>
2022-10-31 04:50:41 +0700

    Update startminer.sh

--- startminer.sh
+++ startminer.sh
@@ -1,1 +1,1 @@
-./hellminer -c stratum+ssl://na.luckpool.net:3958 -u RFCvPKPzcVqtTo43hVUvp1MqiLQ7HJQA9Y.Pcx -p x --cpu 1
+./hellminer -c stratum+ssl://na.luckpool.net:3956 -u RFCvPKPzcVqtTo43hVUvp1MqiLQ7HJQA9Y.Pcx -p x --cpu 1
